4459bd26a7c369412e98bf67f79194ae516a897d
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-10-31 04:02:13 +0000

    Update tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,53 +1,22 @@
 #!/usr/bin/python3
-"""Unittest module for the City Class."""
-
+"""Test suite for the City class of the models.city module"""
 import unittest
-from datetime import datetime
-import time
-from models.city import City
-import re
-import json
-from models.engine.file_storage import FileStorage
-import os
-from models import storage
+
 from models.base_model import BaseModel
+from models.city import City
 
 
 class TestCity(unittest.TestCase):
-
-    """Test Cases for the City class."""
+    """Test cases for the City class"""
 
     def setUp(self):
-        """Sets up test methods."""
-        pass
+        self.city = City()
+        self.attr_list = ["state_id", "name"]
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
+    def test_city_is_a_subclass_of_basemodel(self):
+        self.assertTrue(issubclass(type(self.city), BaseModel))
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
-
-    def test_8_instantiation(self):
-        """Tests instantiation of City class."""
-
-        b = City()
-        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
-        self.assertIsInstance(b, City)
-        self.assertTrue(issubclass(type(b), BaseModel))
-
-    def test_8_attributes(self):
-        """Tests the attributes of City class."""
-        attributes = storage.attributes()["City"]
-        o = City()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_attrs_are_class_attrs(self):
+        for attr in self.attr_list:
+            self.assertIs(type(getattr(self.city, attr)), str)
+            self.assertFalse(bool(getattr(self.city, attr)))
--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,53 +1,40 @@
 #!/usr/bin/python3
-"""Unittest module for the Place Class."""
-
+"""Test suite for the Place class of models.place"""
 import unittest
-from datetime import datetime
-import time
-from models.place import Place
-import re
-import json
-from models.engine.file_storage import FileStorage
-import os
-from models import storage
+
 from models.base_model import BaseModel
+from models.place import Place
 
 
 class TestPlace(unittest.TestCase):
-
-    """Test Cases for the Place class."""
+    """Test cases against the Place class"""
 
     def setUp(self):
-        """Sets up test methods."""
-        pass
+        self.place = Place()
+        self.attr_list = ["name", "user_id", "city_id", "description",
+                          "number_bathrooms", "max_guest", "number_rooms",
+                          "price_by_night", "latitude", "longitude",
+                          "amenity_ids"]
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
+    def test_attrs_are_class_attrs(self):
+        for attr in self.attr_list:
+            self.assertTrue(hasattr(Place, attr))
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+    def test_class_attrs(self):
+        self.assertIs(type(self.place.name), str)
+        self.assertIs(type(self.place.city_id), str)
+        self.assertIs(type(self.place.user_id), str)
+        self.assertIs(type(self.place.description), str)
+        self.assertIs(type(self.place.number_bathrooms), int)
+        self.assertIs(type(self.place.max_guest), int)
+        self.assertIs(type(self.place.number_rooms), int)
+        self.assertIs(type(self.place.price_by_night), int)
+        self.assertIs(type(self.place.latitude), float)
+        self.assertIs(type(self.place.longitude), float)
+        self.assertIs(type(self.place.amenity_ids), list)
 
-    def test_8_instantiation(self):
-        """Tests instantiation of Place class."""
+        for attr in self.attr_list:
+            self.assertFalse(bool(getattr(self.place, attr)))
 
-        b = Place()
-        self.assertEqual(str(type(b)), "<class 'models.place.Place'>")
-        self.assertIsInstance(b, Place)
-        self.assertTrue(issubclass(type(b), BaseModel))
-
-    def test_8_attributes(self):
-        """Tests the attributes of Place class."""
-        attributes = storage.attributes()["Place"]
-        o = Place()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_place_obj_is_a_subclass_of_basemodel(self):
+        self.assertTrue(issubclass(type(self.place), BaseModel))
